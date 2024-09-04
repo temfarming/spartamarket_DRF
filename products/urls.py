5a629a497a7b62c3da3506653adc76e2b54c6b84
temfarming <temfarming@naver.com>
2024-09-04 16:41:14 +0900

    쓸데 없는 코드 및 파일들 삭제 및 본격적인 CRUD 코드 작성

--- products/urls.py
+++ products/urls.py
@@ -3,9 +3,4 @@ from . import views
 
 app_name = "products"
 urlpatterns = [
-    path("html/", views.product_list_html, name = "product_list_html"),
-    path("json-01/", views.json_01, name="json_01"),
-    path("json-02/", views.json_02, name="json_02"),
-    path("json-drf/", views.json_drf, name="json_drf"),
-    
 ]
--- products/views.py
+++ products/views.py
@@ -6,36 +6,4 @@ from rest_framework.response import Response
 from .models import Product
 from .serializers import ProductSerializer
 
-# Create your views here.
-def product_list_html(request):
-    products = Product.objects.all()
-    context = {"products":products}
-    return render(request, "products/product_list.html", context)
 
-def json_01(request):
-    products = Product.objects.all()
-    json_products = []
-
-    for product in products:
-        json_products.append(
-            {            
-            "title" : product.title,
-            "content" : product.content,
-            "created_at" : product.created_at,
-            "updated_at" : product.updated_at,
-
-            }
-        )
-
-    return JsonResponse(json_products, safe=False)
-
-def json_02(request):
-    products = Product.objects.all()
-    res_data = serializers.serialize("json", products)
-    return HttpResponse(res_data, content_type="application/json")
-
-@api_view(["GET"])
-def json_drf(request):
-    products = Product.objects.all()
-    serializer = ProductSerializer(products, many=True)
-    return Response(serializer.data)
