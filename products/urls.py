6d6661054024a06aa3ab932b75c573a25f35062f
temfarming <temfarming@naver.com>
2024-09-04 18:02:11 +0900

    product 목록조회, 상세조회, 수정, 생성, 삭제 API 생성

--- products/urls.py
+++ products/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 app_name = "products"
 urlpatterns = [
+    path("", views.product_list, name = "product_list"),
+    path("<int:pk>/", views.product_detail, name = "product_detail"),
+
 ]
--- products/views.py
+++ products/views.py
@@ -3,7 +3,40 @@ from django.http import JsonResponse, HttpResponse
 from django.core import serializers
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from django.shortcuts import get_object_or_404
 from .models import Product
 from .serializers import ProductSerializer
 
 
+@api_view(['GET', 'POST'])
+def product_list(request):
+    if request.method == "GET":
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+    
+    elif request.method == "POST":
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def product_detail(request, pk):
+    if request.method == "GET":
+        product = get_object_or_404(Product, pk=pk)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    
+    elif request.method == "PUT":
+        product = get_object_or_404(Product, pk=pk)
+        serializer = ProductSerializer(product, data=request.data, partial=True)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+    
+    elif request.method == "DELETE":
+        product = get_object_or_404(Product, pk=pk)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
