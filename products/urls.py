56ceac4f9931d395964ca0924a56449fc0762c1c
temfarming <temfarming@naver.com>
2024-09-04 19:38:12 +0900

    product 관련 view내에서 사용중인 CRUD, class로 변환

--- products/urls.py
+++ products/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name = "products"
 urlpatterns = [
-    path("", views.product_list, name = "product_list"),
-    path("<int:pk>/", views.product_detail, name = "product_detail"),
+    path("", views.ProductListAPIView.as_view(), name = "product_list"),
+    path("<int:pk>/", views.ProductDetailAPIView.as_view(), name = "product_detail"),
 
 ]
--- products/views.py
+++ products/views.py
@@ -5,38 +5,41 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
 from .models import Product
 from .serializers import ProductSerializer
 
-
-@api_view(['GET', 'POST'])
-def product_list(request):
-    if request.method == "GET":
+    
+class ProductListAPIView(APIView):
+    def get(self, request):
         products = Product.objects.all()
         serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
     
-    elif request.method == "POST":
+    def post(self, request):
         serializer = ProductSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def product_detail(request, pk):
-    if request.method == "GET":
-        product = get_object_or_404(Product, pk=pk)
+class ProductDetailAPIView(APIView):
+
+    def get_object(self, pk):
+        return get_object_or_404(Product, pk=pk)
+    
+    def get(self, request, pk):
+        product = self.get_object(pk)
         serializer = ProductSerializer(product)
         return Response(serializer.data)
     
-    elif request.method == "PUT":
-        product = get_object_or_404(Product, pk=pk)
+    def put(self, request, pk):
+        product = self.get_object(pk)
         serializer = ProductSerializer(product, data=request.data, partial=True)
         if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data)
-    
-    elif request.method == "DELETE":
-        product = get_object_or_404(Product, pk=pk)
+        
+    def delete(self, request, pk):
+        product = self.get_object(pk)
         product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
