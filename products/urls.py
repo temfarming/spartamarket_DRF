52278e8a025a04b18a308faecbba8aad3a7df887
temfarming <temfarming@naver.com>
2024-09-04 13:38:10 +0900

    view 함수 product_list_html 생성 및 product_list.html 페이지 생성. 테스트 완료.

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from . import views
 
-
+app_name = "products"
 urlpatterns = [
+    path("html/", views.product_list_html, name = "product_list_html"),
     
 ]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+def product_list_html(request):
+    products = Product.objects.all()
+    context = {"products":products}
+    return render(request, "products/product_list.html", context)
