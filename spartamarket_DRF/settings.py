82886316c3c8bbe3b9b40d66ab3c2aff52db819c
temfarming <temfarming@naver.com>
2024-09-05 09:54:12 +0900

    accounts앱 생성 및 setting에 앱 등록, AUTH 만들어 주고, simplejwt 설치 완료

--- spartamarket_DRF/settings.py
+++ spartamarket_DRF/settings.py
@@ -87,6 +87,9 @@ DATABASES = {
     }
 }
 
+# Custom User Model
+AUTH_USER_MODEL = "accounts.User"
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
